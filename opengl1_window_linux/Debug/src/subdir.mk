fc19c0c62426590cc03da7c4cf18b1ccf6ba33dc
appalse <natalia.kutina13@gmail.com>
2018-05-07 20:28:22 +0300

    add *.mk files

---
+++ opengl1_window_linux/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/main.cpp 
+
+OBJS += \
+./src/main.o 
+
+CPP_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__cplusplus=201103L -D__GXX_EXPERIMENTAL_CXX0X__ -I/home/kar/Documents/c++/eclipse/opengl1_window_linux/3rd-party/include -O0 -g3 -Wall -c -fmessage-length=0  -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
